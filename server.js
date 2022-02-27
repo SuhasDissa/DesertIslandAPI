bb029144f9ed0b2b3953c1b34865a69741bda40f
Suhas Dissanayake <suhasdissa@protonmail.com>
2022-02-27 17:25:08 +0530

    Fix CORS error

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const compression = require('compression');
 const bodyParser = require('body-parser');
 
+app.use(cors());
 app.use(express.json());
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: false }));
